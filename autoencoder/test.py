6d63f47cbc2aeca30e3918b2dfd7cab6c12c1327
kwjinwoo <jung003327@gmail.com>
2022-01-07 09:32:41 +0900

    Update Make dataset

--- autoencoder/test.py
+++ autoencoder/test.py
@@ -1,2 +1,10 @@
 import tensorflow as tf
-from tensorflow.keras.utils import plot_model
+import data_util
+import glob
+
+
+t, d = data_util.get_autoencoder_dataset()
+
+for i in t.take(1):
+    print(i)
+    break
