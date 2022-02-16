9933e8eb96b815d904a4f5e38ce9e7766270df63
kwjinwoo <jung003327@gmail.com>
2022-02-16 14:32:20 +0900

    Update mobilenet AE

--- autoencoder/inverse_mobilenet.py
+++ autoencoder/inverse_mobilenet.py
@@ -14,33 +14,25 @@ class DecoderBlock(keras.Model):
         self.stride = stride
         self.conv1 = keras.layers.Conv2D(self.filters, kernel_size=1, padding='same', use_bias=False)
         self.bn1 = keras.layers.BatchNormalization()
-        self.deconv = keras.layers.Conv2DTranspose(self.filters, kernel_size=3,
-                                                   padding='same', strides=self.stride, activation='relu',
+        self.deconv = keras.layers.Conv2DTranspose(self.filters, kernel_size=4,
+                                                   padding='same', strides=self.stride,
                                                    use_bias=False)
-        self.depth_conv = keras.layers.DepthwiseConv2D(kernel_size=3, padding='same', strides=self.stride, activation='relu',
-                                                       use_bias=False)
         self.bn2 = keras.layers.BatchNormalization()
         self.conv2 = keras.layers.Conv2D(out_channels, kernel_size=1, padding='same', use_bias=False)
         self.bn3 = keras.layers.BatchNormalization()
 
     def call(self, x):
-        residual = x
         x = self.conv1(x)
         x = self.bn1(x)
         x = self.relu(x)
 
-        if self.stride == 1:
-            x = self.depth_conv(x)
-        else:
-            x = self.deconv(x)
+        x = self.deconv(x)
         x = self.bn2(x)
         x = self.relu(x)
 
         x = self.conv2(x)
         x = self.bn3(x)
-
-        if self.stride == 1 and self.in_channels == self.out_channels:
-            x += residual
+        x = self.relu(x)
 
         return x
 
@@ -51,7 +43,6 @@ class InverseMobileNetV2(keras.Model):
         super(InverseMobileNetV2, self).__init__()
         self.z_dim = z_dim
         self.latent_dim = input_size // 2 ** 5
-        # self.fc = keras.layers.Dense(self.latent_dim * self.latent_dim * 1280)
         self.relu = keras.layers.LeakyReLU(0.2)
         self.deconv1 = keras.layers.Conv2D(320, 1, use_bias=False)
         self.bn1 = keras.layers.BatchNormalization()
@@ -60,8 +51,6 @@ class InverseMobileNetV2(keras.Model):
         self.bn2 = keras.layers.BatchNormalization()
 
     def call(self, x):
-        # out = self.fc(x)
-        # out = keras.layers.Reshape((self.latent_dim, self.latent_dim, 1280))(out)
         out = self.deconv1(x)
         out = self.bn1(out)
         out = self.relu(out)
@@ -75,20 +64,10 @@ class InverseMobileNetV2(keras.Model):
         config = [
             [320, 160, 1],
             [160, 96, 2],
-            [96, 96, 1],
-            [96, 96, 1],
             [96, 64, 1],
-            [64, 64, 1],
-            [64, 64, 1],
             [64, 32, 2],
-            [32, 32, 1],
-            [32, 32, 1],
-            [32, 32, 1],
             [32, 24, 2],
-            [24, 24, 1],
-            [24, 24, 1],
             [24, 16, 2],
-            [16, 16, 1],
             [16, 32, 1]
         ]
         blocks = keras.models.Sequential(
@@ -103,29 +82,48 @@ class MobileNetAE(keras.Model):
         super(MobileNetAE, self).__init__()
         self.input_size = input_size
         self.z_dim = z_dim
-        # self.fc = keras.layers.Dense(z_dim)
-        self.encoder = self._make_encoder()
-        self.decoder = InverseMobileNetV2(self.input_size[0], self.z_dim)
+        self.latent_size = input_size[0] // 2 ** 5
+        self.AE = self._make_AE()
+
+        # self.encoder = self._make_encoder()
+        # self.decoder = InverseMobileNetV2(self.input_size[0], self.z_dim)
 
     def call(self, x):
         out = self.encoder(x)
         out = self.decoder(out)
         return out
 
-    def _make_encoder(self):
-        # back_input = keras.layers.Input(shape=self.input_size)
-        backbone = keras.applications.MobileNetV2(input_shape=self.input_size, alpha=1.0, include_top=False, weights=None)
-        # backbone = backbone(back_input)
-        # encoder = keras.layers.Flatten()(backbone)
-        # encoder = self.fc(encoder)
-        # encoder = keras.models.Model(back_input, encoder)
-        return backbone
+    def _make_AE(self):
+        encoder = keras.applications.MobileNetV2(input_shape=self.input_size, alpha=1.0, include_top=False, weights=None)
 
+        c5 = [layer for layer in encoder.layers if layer.name == 'block_16_project_BN'][0].output
+        c4 = [layer for layer in encoder.layers if layer.name == 'block_12_add'][0].output
+        c3 = [layer for layer in encoder.layers if layer.name == 'block_5_add'][0].output
+        c2 = [layer for layer in encoder.layers if layer.name == 'block_2_add'][0].output
+        c1 = [layer for layer in encoder.layers if layer.name == 'expanded_conv_project_BN'][0].output
+
+        print(c1.shape)
+        return encoder
+
+    def _make_decoder_blocks(self):
+        config = [
+            [320, 160, 1],
+            [160, 96, 2],
+            [96, 64, 1],
+            [64, 32, 2],
+            [32, 24, 2],
+            [24, 16, 2],
+            [16, 32, 1]
+        ]
+        blocks = keras.models.Sequential(
+            [DecoderBlock(i[0], i[1], i[2]) for i in config]
+        )
+        return blocks
 
 ae = MobileNetAE((256, 256, 3), 99)
-temp = tf.random.normal(shape=(1, 256, 256, 3))
-out = ae(temp)
-print(out.shape)
-print(ae.summary())
+# temp = tf.random.normal(shape=(1, 256, 256, 3))
+# out = ae(temp)
+# print(out.shape)
+# print(ae.summary())
 
 
---
+++ autoencoder/mobilenet_ae_test.py
@@ -0,0 +1,98 @@
+from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2
+from tensorflow.keras.layers import Conv2D, Dropout, Dense, Conv2DTranspose, BatchNormalization, Flatten, concatenate, Reshape
+from tensorflow.keras.models import Model
+from tensorflow.keras import backend as K
+import numpy as np
+
+
+class MobileNetV2_UNet:
+    def __init__(self, input_shape=(256, 256, 3), read_image_func=None, encoder_output_size=None):
+        super().__init__()
+        self.dice_coefficient = False
+        self.model = None
+        self.input_shape = input_shape
+        self.read_image_func = read_image_func
+        self.encoder_output_size = encoder_output_size
+
+        if encoder_output_size is not None:
+            self.encoder_last_layer_name = "latent-space-rep"
+        else:
+            self.encoder_last_layer_name = "block_16_project_BN"
+        self.encoder = None
+
+    def build_model(self, nodes):
+        mobileNet = MobileNetV2(input_shape=self.input_shape, include_top=False)
+        inputs = mobileNet.inputs
+
+        c5 = [layer for layer in mobileNet.layers if layer.name == 'block_16_project_BN'][0].output
+        c4 = [layer for layer in mobileNet.layers if layer.name == 'block_12_add'][0].output
+        c3 = [layer for layer in mobileNet.layers if layer.name == 'block_5_add'][0].output
+        c2 = [layer for layer in mobileNet.layers if layer.name == 'block_2_add'][0].output
+        c1 = [layer for layer in mobileNet.layers if layer.name == 'expanded_conv_project_BN'][0].output
+
+        # c5 = [layer for layer in mobileNet.layers if layer.name == 'block_16_project'][0].output
+        # c4 = [layer for layer in mobileNet.layers if layer.name == 'block_12_project'][0].output
+        # c3 = [layer for layer in mobileNet.layers if layer.name == 'block_5_project'][0].output
+        # c2 = [layer for layer in mobileNet.layers if layer.name == 'block_2_project'][0].output
+        # c1 = [layer for layer in mobileNet.layers if layer.name == 'Conv1'][0].output
+
+        bottleneck = Conv2D(1280, 1, use_bias=False, padding='same')(c5)
+        c5 = BatchNormalization()(bottleneck)
+        if self.encoder_output_size is not None:
+            volumeSize = K.int_shape(c5)
+            x = Flatten()(c5)
+            x = Dense(self.encoder_output_size, name="latent-space-rep")(x)
+            x = Dense(np.prod(volumeSize[1:]))(x)
+            c5 = Reshape((volumeSize[1], volumeSize[2], volumeSize[3]))(x)
+        c5 = Conv2DTranspose(320, 1, use_bias=False, padding='same')(c5)
+        c5 = BatchNormalization()(c5)
+
+        # DECODER Unet
+        u6 = Conv2DTranspose(nodes * 8, (2, 2), strides=(2, 2), padding='same')(c5)
+        u6 = concatenate([u6, c4])
+        c6 = Conv2D(nodes * 8, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same')(u6)
+        c6 = BatchNormalization()(c6)
+        c6 = Dropout(0.2)(c6)
+        c6 = Conv2D(nodes * 8, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same')(c6)
+        c6 = BatchNormalization()(c6)
+
+        u7 = Conv2DTranspose(nodes * 4, (2, 2), strides=(2, 2), padding='same')(c6)
+        u7 = concatenate([u7, c3])
+        c7 = Conv2D(nodes * 4, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same')(u7)
+        c7 = BatchNormalization()(c7)
+        c7 = Dropout(0.2)(c7)
+        c7 = Conv2D(nodes * 4, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same')(c7)
+        c7 = BatchNormalization()(c7)
+
+        u8 = Conv2DTranspose(nodes * 2, (2, 2), strides=(2, 2), padding='same')(c7)
+        u8 = concatenate([u8, c2])
+        c8 = Conv2D(nodes * 2, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same')(u8)
+        c8 = BatchNormalization()(c8)
+        c8 = Dropout(0.1)(c8)
+        c8 = Conv2D(nodes * 2, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same')(c8)
+        c8 = BatchNormalization()(c8)
+
+        u9 = Conv2DTranspose(nodes, (2, 2), strides=(2, 2), padding='same')(c8)
+        u9 = concatenate([u9, c1], axis=3)
+        c9 = Conv2D(nodes, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same')(u9)
+        c9 = BatchNormalization()(c9)
+        c9 = Dropout(0.1)(c9)
+        c9 = Conv2D(nodes, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same')(c9)
+        c9 = BatchNormalization()(c9)
+
+        u10 = Conv2DTranspose(nodes, (2, 2), strides=(2, 2), padding='same', name='last_block_transponse')(c9)
+        #         u10 = concatenate([u9, c1], axis=3)
+        c10 = Conv2D(nodes, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same')(u10)
+        c10 = BatchNormalization()(c10)
+        c10 = Dropout(0.1)(c10)
+        c10 = Conv2D(nodes, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same')(c10)
+        c10 = BatchNormalization()(c10)
+
+        outputs = Conv2D(3, (1, 1), activation='sigmoid')(c10)
+
+        self.model = Model(inputs=[inputs], outputs=[outputs])
+        return self.model
+
+
+ae = MobileNetV2_UNet().build_model(20)
+ae.summary()
